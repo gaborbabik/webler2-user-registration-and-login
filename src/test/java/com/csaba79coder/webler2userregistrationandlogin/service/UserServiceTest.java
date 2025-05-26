b4a517f33aa8f5e2d1ea4acb93381ba586fd4ca8
Gábor Bábik <gaborbabik@gmail.com>
2025-05-26 18:29:35 +0200

    add UserServiceTest

---
+++ src/test/java/com/csaba79coder/webler2userregistrationandlogin/service/UserServiceTest.java
@@ -0,0 +1,20 @@
+package com.csaba79coder.webler2userregistrationandlogin.service;
+
+import com.csaba79coder.webler2userregistrationandlogin.persistence.UserRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private UserService userService;
+
+
+}
